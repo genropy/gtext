c34f26eaf04d931020a6decb0eaf6a4e1c345bf5
Giovanni Porcari <giovanni.porcari@gmail.com>
2025-11-02 20:40:03 +0100

    test: Verify Codecov GitHub App integration

--- gtext/__init__.py
+++ gtext/__init__.py
@@ -1,6 +1,7 @@
 """gtext - The text wizard: Transform text files with pluggable extensions."""
 
 __version__ = "0.2.0"
+# Test Codecov integration
 __author__ = "Genropy Team"
 
 from gtext.processor import TextProcessor
